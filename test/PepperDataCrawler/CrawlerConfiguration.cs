a390bb049a02486fa28fd57be48c14b152587e23
Pham Van Truong <utcntt@gmail.com>
2017-01-09 22:17:11 +0700

    Add a project for crawling Black Pepper Data.

---
+++ test/PepperDataCrawler/CrawlerConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TestLibrary;
+using APICore;
+
+namespace PepperDataCrawler
+{
+    public class CrawlerConfiguration : TestConfig
+    {
+        public static string KnowledgeBaseID
+        {
+            get
+            {
+                return GetValueFromConfig("KnowledgeBaseID");
+            }
+        }
+
+        public static string SubscriptionKey
+        {
+            get
+            {
+                return GetValueFromConfig("SubscriptionKey");
+            }
+        }
+    }
+}
